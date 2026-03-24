24c39373a4ef5fef8c8638d6a2cefc361a9da3aa
Evan-Downs420 <evan.downs@stu.ocsb.ca>
2026-03-24 16:07:59 -0400

    wasd

--- main.ts
+++ main.ts
@@ -1,8 +1,33 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Evan
+ * Created on: Mar 2026
+ * This program makes 2 random integers from 0-99
 */
 
-basic.showString('Hello, World!')
+let first_number = 0
+let second_number = 0
+
+// Generate random integers on start
+first_number = randint(0, 99)
+second_number = randint(0, 99)
+
+input.onButtonPressed(Button.A, function () {
+    basic.showNumber(first_number)
+    basic.clearScreen()
+})
+
+input.onButtonPressed(Button.B, function () {
+    basic.showNumber(second_number)
+    basic.clearScreen()
+})
+
+input.onGesture(Gesture.Shake, function () {
+    if (first_number < second_number) {
+        // If first is 10 and second is 20, shows "10 < 20"
+        basic.showString(first_number + " < " + second_number)
+    } else {
+        // If first is 50 and second is 5, shows "50 > 5"
+        basic.showString(first_number + " > " + second_number)
+    }
+})
